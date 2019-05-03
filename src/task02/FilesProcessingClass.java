26e5dfec5aad13039a91cf70bcf21306f4d23463
Ilia Gavrilov <elijahgavri@gmail.com>
2019-05-04 00:57:05 +0300

    sixth commit, task02 methods created, files created, methods described

--- src/task02/FilesProcessingClass.java
+++ src/task02/FilesProcessingClass.java
@@ -1,6 +1,5 @@
 package task02;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,6 +9,7 @@ import java.util.List;
 import java.util.Random;
 
 public class FilesProcessingClass {
+    /*Основные и вспомогательные поля класса для генерирования файлов*/
     List<String> words = new ArrayList<>();
     List<String> initialWords = new ArrayList<>();
     String previousSentence = "";
@@ -19,9 +19,9 @@ public class FilesProcessingClass {
     String sentenceEnd =  "(.|!|?)+\"\"";
     String alphabet =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     List<String> previousSentenceWords = new ArrayList<>();
-
     ArrayList<FileOutputStream> multipleFos = new ArrayList<>();
 
+    /* Конструктор класса */
     public FilesProcessingClass(List<String> initialWords, List<String> words){
         this.initialWords = initialWords;
         this.words = words;
@@ -35,7 +35,6 @@ public class FilesProcessingClass {
         previousSentence = previousSentence + alphabet.charAt(rand.nextInt(alphabet.length())) + " ";
         for (int i=0; i<n-1; i++) {
             word =  initialWords.get(rand.nextInt(initialWords.size()));
-            //word =  words.get(rand.nextInt(words.size()));
             previousSentence = previousSentence + word;
             previousSentence = previousSentence + commaSpace.get(rand.nextInt(commaSpace.size()));
             previousSentenceWords.add(word);
@@ -44,37 +43,26 @@ public class FilesProcessingClass {
         previousSentence = previousSentence + words.get(rand.nextInt(words.size()));
         previousSentence = previousSentence + sentenceEnd.charAt(rand.nextInt(sentenceEnd.length()));
         previousSentenceWords.add(word);
-        //previousSentenceWords.clear();
-        //return previousSentence;
     }
 
     /* Метод генерирования абзацев */
     public void paragraphGenerator(int n1, int n2, double givenProb){
         for (int i=0; i<n2; i++){
-            //paragraph = "";
             sentenceGenerator(n1);
             paragraph = paragraph + previousSentence;
             paragraph = paragraph + " ";
             if (probsGenerator(givenProb)) {
-
-                //String x = previousSentenceWords.get(rand.nextInt(previousSentenceWords.size()));
                 String x = words.get(rand.nextInt(words.size()));
-
                 //System.out.println("Слово: " + x + " из массива words");
                 //System.out.println("Из предыдущего предложения: "+ previousSentence);
-
                 sentenceGenerator(n1);
                 String newSentence = previousSentence.substring(0, previousSentence.length() - 2) + " " + x + sentenceEnd.charAt(rand.nextInt(sentenceEnd.length())) + " ";
                 paragraph = paragraph + newSentence;
-                //paragraph = paragraph + previousSentence.substring(0, previousSentence.length() - 2);
-                //paragraph = paragraph + " " + x + sentenceEnd.charAt(rand.nextInt(sentenceEnd.length())) + " ";
                 //System.out.println("Попало в предложение " + previousSentence);
                 //System.out.println("Попало в предложение: " + newSentence);
                 //System.out.println("Вероятность попадания задана как " + givenProb + "\n");
                 i++;
             }
-            //paragraph = paragraph + previousSentence;
-            //paragraph = paragraph + " ";
         }
         paragraph = paragraph + "\r\n";
     }
@@ -87,19 +75,19 @@ public class FilesProcessingClass {
         } else { return false; }
     }
 
-    /* Определение размера файлов величиной size */
-    //public void getFiles(String path,  int size, String[] words, int probability) {
-    public void getFiles(String path, int n, int size, int probability, String[] words) {//, String[] words){//, ) {
+    /* Основной метод класса по генерированию по условиям:
+    * - задается каталог для сохранения файлов;
+    * - определяется количество генерируемых файлов;
+    * - генерируются файлы заданного размера;
+    * - задается вероятность попадания слов из массива words в каждое предложение;
+    * - передается массив words */
+    public void getFiles(String path, int n, int size, int probability, String[] words) {
         setWords(Arrays.asList(words));
         for (int i = 0; i < n; i++){
             byte[] buffer = new byte[0];
             int x = 0;
-            //paragraphGenerator(15, 20, (double) probability / 100);
             FileOutputStream y = serialisation(path, i);
             while (x < size) {
-                //try (serialisation(path, i)) {
-                //try (FileOutputStream fos = new FileOutputStream(path + "fileNumber" + i+1 + ".txt")) {
-                    //FileOutputStream y = serialisation(path, i);
                     paragraphGenerator(15, 20, (double) probability / 100);
                     buffer = paragraph.getBytes();
                     x = buffer.length;
@@ -108,34 +96,24 @@ public class FilesProcessingClass {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                //paragraphGenerator(15, 20, (double) probability / 100);
                 paragraph = "";
                 paragraphGenerator(15, 20, (double) probability / 100);
-                //fos.write(buffer, 0, size);
-                    //x = buffer.length;
-                    System.out.println(x);
-                    //paragraphGenerator(15, 20, (double) probability / 100);
-                    //break;
-                    //fos.close();
-                //} catch (IOException ex) {
-                  //  System.out.println(ex.getMessage());
-                }
-                //fos.close();
+                    //System.out.println(x);
             }
         }
-    //}
+    }
 
+    /* Вспомогательный метод для сериализации сохранения файлов */
     public FileOutputStream serialisation(String path, int i) {
-
         try {
             multipleFos.add(new FileOutputStream(path + "fileNumber" + (i+1) + ".txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        //FileOutputStream fos = new FileOutputStream(path + "fileNumber" + i + ".txt"
         return multipleFos.get(i);
     }
 
+    /* Вспомогательный сеттер для назначения массива words в поле класса */
     public void setWords(List<String> words) {
         this.words = words;
     }
--- src/task02/Main.java
+++ src/task02/Main.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-//import java.net.MalformedURLException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         /* Создание массива слов initialWords размерностью <=1000 для генерирования предложений */
         URL givenLink = new URL("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears-medium.txt");
         BufferedReader inFirst = new BufferedReader(new InputStreamReader(givenLink.openStream()));
@@ -20,12 +19,8 @@ public class Main {
             inputLineFirst = inFirst.readLine();
             initialWords.add(inputLineFirst);
         }
-//        while ((inputLineFirst = inFirst.readLine()) != null) {
-//            initialWords.add(inputLineFirst);
-//        }
-
         inFirst.close();
-        //System.out.println("Массив слов initialWords размерностью " + initialWords.size() + ": " + initialWords);
+        System.out.println("Массив слов initialWords размерностью " + initialWords.size() + ": " + initialWords);
 
         /* Создание массива слов words размерностью <=1000 для реализации вероятности */
         URL givenLinkWords = new URL("https://gist.githubusercontent.com/deekayen/4148741/raw/01c6252ccc5b5fb307c1bb899c95989a8a284616/1-1000.txt");
@@ -36,18 +31,16 @@ public class Main {
             words.add(inputLineSecond);
         }
         inSecond.close();
-        //System.out.println("Массив слов words размерностью " + words.size() + ": " + words + "\r\n");
+        System.out.println("Массив слов words размерностью " + words.size() + ": " + words + "\r\n");
 
-        String[] array = words.stream().toArray(String[]::new);// .toArray(words);
-
+        /* Инстанцирование основного класса программы */
         FilesProcessingClass x = new FilesProcessingClass(initialWords, words);
 
         x.paragraphGenerator(15,20, 0.9);
         //System.out.println(x.paragraph);
-        x.getFiles("C:\\Users\\Gavrilov\\IdeaProjects\\part01.lesson06\\src\\task02\\", 3, 3072, 50, array);
 
-//        String givenLink = "https://gist.githubusercontent.com/deekayen/4148741/raw/01c6252ccc5b5fb307c1bb899c95989a8a284616/1-1000.txt";
-//        WordsProcessing wordsToProcess = new WordsProcessing(givenLink);
-//        List<String> words = wordsToProcess.getWords();
+        /* Вызов основного метода класса для генерирования файлов */
+        String[] array = words.stream().toArray(String[]::new);
+        x.getFiles("C:\\Users\\Gavrilov\\IdeaProjects\\part01.lesson06\\src\\task02\\", 3, 3072, 50, array);
     }
 }
